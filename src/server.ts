4fbdda54847ed132bd8824e12bd97f68628ab43f
dannielss <daniel_sousa.p@outlook.com>
2023-01-29 20:58:35 -0300

    feat: running server with trpc endpoint and playground endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,32 @@
+import * as trpcExpress from '@trpc/server/adapters/express';
 import express from 'express';
+import { expressHandler } from 'trpc-playground/handlers/express';
+import router from './router';
 
-const app = express();
+const trpcEndpoint = '/trpc';
+const playgroundEndpoint = '/playground'
 
-app.use('/', (req, res) => {
-    res.send("Hello")
-});
+const runApp = async () => {
+    const app = express();
+    app.use(express.json());
+    
+    app.use(
+        trpcEndpoint,
+        trpcExpress.createExpressMiddleware({
+            router
+        })
+    );
 
-app.listen(3000, () => console.log('Server is running'))
+    app.use(
+        playgroundEndpoint,
+        await expressHandler({
+            trpcApiEndpoint: trpcEndpoint,
+            playgroundEndpoint,
+            router
+        })
+    );
+    
+    app.listen(3000, () => console.log('Server is running'));
+}
+
+runApp();
